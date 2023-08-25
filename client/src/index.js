1a93ae83a56e1498f9cd5acaab1d8dfa77cfcd85
elisemcclain <celisemcclain@gmail.com>
2023-08-25 15:44:44 -0500

    first try

--- client/src/index.js
+++ client/src/index.js
@@ -6,3 +6,10 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
