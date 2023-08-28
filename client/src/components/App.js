5dcc43c191c45d9a19523b6b78c3ea0edd40311b
Grant Bruemmer <grantbruemmer@gmail.com>
2023-08-28 15:31:08 -0500

    Test Push

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-import Goblin from "./Goblin";
-import Home from "./Home";
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-// const API = "http://localhost:3000";
+import Goblin from "./Goblin";
+import Login from "./Login";
+import Home from "./Home";
 
 function App() {
   return (
-    <main>
-      <h1>Phase 4 Project Client</h1>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/goblins/:id">
-          <Goblin />
-        </Route>
-      </Switch>
-    </main>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/goblin">
+            <Goblin />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,16 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react-router";
 
 function Home() 
 //     pass
+function Home() {
+  //   print("hello Goblin");
 
+  return (
+    <div>
+      <h1>*Goblin Love*</h1>
+    </div>
+  );
+}
 
-// return(
-
-// )}
+export default Home;
