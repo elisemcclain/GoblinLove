49ac5fa319759e00568fc27452fddfe69bf8c6f1
Grant Bruemmer <grantbruemmer@gmail.com>
2023-08-29 12:23:26 -0500

    changes to Login on elise branch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Goblin from "./Goblin";
-import Login from "./Login";
+// import Login from "./Login";
+import Login2 from "./Login2";
 import Home from "./Home";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [goblins, setGoblins] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:5555/login")
+    fetch("http://localhost:5555/users")
       .then((r) => r.json())
       .then((userArray) => {
         setUsers(userArray);
@@ -29,7 +32,16 @@ function App() {
   const handleAddUser = (newUser) => {
     const updatedUserArray = [...users, newUser];
     setUsers(updatedUserArray);
+    setCurrentUser(newUser);
+    setLoggedIn(true);
   };
+
+  const handleLogin = (user) => {
+    console.log(user)
+    setCurrentUser(user);
+    setLoggedIn(true);
+  }
+  
 
   return (
     <BrowserRouter>
@@ -39,11 +51,12 @@ function App() {
             <Home />
           </Route>
           <Route exact path="/login">
-            <Login
+            {/* <Login
               onAddUser={handleAddUser}
               users={users}
               setUsers={setUsers}
-            />
+            /> */}
+            <Login2 users = {users} handleAddUser = {handleAddUser} handleLogin = {handleLogin}/>
           </Route>
           <Route exact path="/goblin">
             <Goblin />
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,101 +3,99 @@ import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
 export const Login = ({ users, setUsers }) => {
-  const [refreshPage, setRefreshPage] = useState(false);
-  const [userExists, setUserExists] = useState(false);
+    const [refreshPage, setRefreshPage] = useState(false);
+    const [userExists, setUserExists] = useState(false);
 
-  useEffect(() => {
-    console.log("fetched");
-    fetch("http://127.0.0.1:5555/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-        console.log({ data });
-      });
-  }, [refreshPage]);
+    useEffect(() => {
+        console.log("fetched");
+        fetch("http://127.0.0.1:5555/users")
+            .then((res) => res.json())
+            .then((data) => {
+                setUsers(data);
+                console.log({ data });
+            });
+    }, [refreshPage]);
 
-  const formSchema = yup.object().shape({
-    email: yup.string().email("Invalid email").required("Must enter email"),
-    username: yup.string().required("Must enter a username").max(20),
-    password: yup.string().required("Must enter a password").max(20),
-  });
+    const formSchema = yup.object().shape({
+        email: yup.string().email("Invalid email").required("Must enter email"),
+        username: yup.string().required("Must enter a username").max(20),
+        password: yup.string().required("Must enter a password").max(20),
+    });
 
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      email: "",
-      password: "",
-    },
-    validationSchema: formSchema,
-    onSubmit: (values) => {
-      fetch("http://127.0.0.1:5555/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            email: "",
+            password: "",
         },
-        body: JSON.stringify(values, null, 2),
-      })
-        .then((res) => {
-          if (res.status == 201) {
-            // account created
-            console.log("account made");
-            setRefreshPage(!refreshPage);
-            setUserExists(true);
-          }
-        })
-        .catch((error) => {
-          console.error("Error:", error);
-        });
-    },
-  });
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            fetch("http://127.0.0.1:5555/users", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(values, null, 2),
+            })
+                .then((res) => {
+                    if (res.status == 201) {
+                        // account created
+                        console.log("account made");
+                        setRefreshPage(!refreshPage);
+                        setUserExists(true);
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error:", error);
+                });
+        },
+    });
 
-  return (
-    <div>
-      <h1>Welcome</h1>
-      <p>Enter your email to login or create an account</p>
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <br />
-
-        <input
-          id="email"
-          name="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <p> {formik.errors.email}</p>
-        <label htmlFor="username">Username</label>
-        <br />
-
-        {userExists ? (
-          <>
-            <label htmlFor="password">Password</label>
-            <br />
-
-            <input
-              id="password"
-              name="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-            <p> {formik.errors.password}</p>
-            <button type="submit">Submit</button>
-          </>
-        ) : (
-          <>
-            <input
-              id="username"
-              username="username"
-              onChange={formik.handleChange}
-              value={formik.values.username}
-            />
-            <p> {formik.errors.username}</p>
-            <button type="submit">Create Account</button>
-          </>
-        )}
-      </form>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>Enter your email to login or create an account</p>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <br />
+                <input
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                <p> {formik.errors.email}</p>
+                <label htmlFor="username">Username</label>
+                <br />
+                {userExists ? (
+                    <>
+                        <label htmlFor="password">Password</label>
+                        <br />
+                        <input
+                            id="password"
+                            name="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                        <p> {formik.errors.password}</p>
+                        <button type="submit">Submit</button>
+                    </>
+                ) : (
+                    <>
+                        <input
+                            id="username"
+                            username="username"
+                            onChange={formik.handleChange}
+                            value={formik.values.username}
+                        />
+                        <p> {formik.errors.username}</p>
+                        <button type="submit">Create Account</button>
+                    </>
+                )}
+            </form>
+        </div>
+    );
 };
 
 export default Login;
