b250b9f62f25ff5b330a1931732f055ae2d9068e
elisemcclain <celisemcclain@gmail.com>
2023-08-30 16:58:22 -0500

    user page

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -140,101 +140,117 @@ function UserPage({ users, currentUser, handleChangeUser, handleDeleteUser }) {
 
   return (
     <div>
-      <img
-        className="gob-head-user"
-        src={"client/public/GoblinHead.png"}
-        alt="GOBSMACKED"
-      />
+      <div>
+        <img
+          className="gob-head-user"
+          src={"client/public/GoblinHead.png"}
+          alt="GOBSMACKED"
+        />
+      </div>
       <div className="bar-user"></div>
       {currentUser ? (
         <div>
-          <button type="button" onClick={EditProfile}>
-            {edit ? "Save Profile" : "Edit Profile"}
-          </button>
-          {edit ? (
-            <div>
-              <form onSubmit={formik.handleSubmit}>
-                <br />
-                <div>
-                  <lable>Email</lable>
-                  <input
-                    type="email"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    {...formik.getFieldProps("email")}
-                  />
-                  {formik.touched.email && formik.errors.email ? (
-                    <div>{formik.errors.email}</div>
-                  ) : null}
-                </div>
-                <br />
-                <div>
-                  <label>Username</label>
-                  <input
-                    type="text"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    {...formik.getFieldProps("username")}
-                  />
-                  {formik.touched.username && formik.errors.username ? (
-                    <div>{formik.errors.username}</div>
-                  ) : null}
-                </div>
-                <br />
-                <div>
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    {...formik.getFieldProps("password")}
-                  />
-                  {formik.touched.password && formik.errors.password ? (
-                    <div>{formik.errors.password}</div>
-                  ) : null}
-                </div>
-                <br />
-              </form>
+          <div className="edit-prof-button custom-edit-button-move">
+            <button
+              type="button"
+              onClick={EditProfile}
+              className="edit-prof-button custom-edit-button"
+            >
+              {edit ? "Save Profile" : "Edit Profile"}
+            </button>
+          </div>
+          <div className="bar-background">
+            {edit ? (
               <div>
-                <label>Personality Traits</label>
-                {traits.map((trait) => (
-                  <div key={trait.id}>
+                <form onSubmit={formik.handleSubmit}>
+                  <br />
+                  <div>
+                    <lable>Email</lable>
                     <input
-                      type="checkbox"
-                      id={`trait-${trait.id}`}
-                      onChange={() => {
-                        formik.setFieldValue(
-                          `traits.${trait.id}.checked`,
-                          !formik.values.traits[trait.id]
-                        );
-                      }}
+                      type="email"
+                      onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
-                      checked={formik.values.traits[trait.id]}
+                      {...formik.getFieldProps("email")}
                     />
-                    <label htmlFor={`trait-${trait.id}`}>{trait.name}</label>
+                    {formik.touched.email && formik.errors.email ? (
+                      <div>{formik.errors.email}</div>
+                    ) : null}
                   </div>
-                ))}
+                  <br />
+                  <div>
+                    <label>Username</label>
+                    <input
+                      type="text"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      {...formik.getFieldProps("username")}
+                    />
+                    {formik.touched.username && formik.errors.username ? (
+                      <div>{formik.errors.username}</div>
+                    ) : null}
+                  </div>
+                  <br />
+                  <div>
+                    <label>Password</label>
+                    <input
+                      type="password"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      {...formik.getFieldProps("password")}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                      <div>{formik.errors.password}</div>
+                    ) : null}
+                  </div>
+                  <br />
+                </form>
+                <div>
+                  <label>Personality Traits</label>
+                  {traits.map((trait) => (
+                    <div key={trait.id}>
+                      <input
+                        type="checkbox"
+                        id={`trait-${trait.id}`}
+                        onChange={() => {
+                          formik.setFieldValue(
+                            `traits.${trait.id}.checked`,
+                            !formik.values.traits[trait.id]
+                          );
+                        }}
+                        onBlur={formik.handleBlur}
+                        checked={formik.values.traits[trait.id]}
+                      />
+                      <label htmlFor={`trait-${trait.id}`}>{trait.name}</label>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ) : (
-            <div>
-              <h3>Username: {currentUser.username}</h3>
-              <h3>Email: {currentUser.email}</h3>
-              <h3>Password: 🤣</h3>
-              <h4>Successful Dates with Grubnub: {currentUser.grubnub_win}</h4>
-              <h4>Successful Dates with Sneezle: {currentUser.sneezle_win}</h4>
-              <h4>Successful Dates with Blort: {currentUser.blort_win}</h4>
-              <h4>Successful Dates with Grimble: {currentUser.grimble_win}</h4>
-              <h4>Successful Dates with Zongo: {currentUser.zongos_win}</h4>
-              <br />
-              <h3>Personality Traits: </h3>
-              <ul>
-                {traitAssociations.map((trait) => {
-                  return <li key={trait.trait.id}>{trait.trait.name}</li>;
-                })}
-              </ul>
-            </div>
-          )}
+            ) : (
+              <div>
+                <h3>Username: {currentUser.username}</h3>
+                <h3>Email: {currentUser.email}</h3>
+                <h3>Password: 🤣</h3>
+                <h4>
+                  Successful Dates with Grubnub: {currentUser.grubnub_win}
+                </h4>
+                <h4>
+                  Successful Dates with Sneezle: {currentUser.sneezle_win}
+                </h4>
+                <h4>Successful Dates with Blort: {currentUser.blort_win}</h4>
+                <h4>
+                  Successful Dates with Grimble: {currentUser.grimble_win}
+                </h4>
+                <h4>Successful Dates with Zongo: {currentUser.zongos_win}</h4>
+                <br />
+                <h3>Personality Traits: </h3>
+                <ul>
+                  {traitAssociations.map((trait) => {
+                    return <li key={trait.trait.id}>{trait.trait.name}</li>;
+                  })}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <div>
