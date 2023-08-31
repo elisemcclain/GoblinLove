fe30b5d4a4cb017c287318748297bb44f04e173a
elisemcclain <celisemcclain@gmail.com>
2023-08-31 09:53:48 -0500

    trying to merge

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,14 +5,14 @@ import GoblinContainer from "./Goblins/GoblinContainer";
 import GoblinDetails from "./Goblins/GoblinDetails";
 import Home from "./Home";
 import Login from "./Login";
-import UserPage from "./UserPage"
-import NavBar from "./NavBar"
+import UserPage from "./UserPage";
+import NavBar from "./NavBar";
+import Game from "./Game";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [goblins, setGoblins] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
-
 
   useEffect(() => {
     fetch("http://localhost:5555/users")
@@ -30,7 +30,7 @@ function App() {
         const goblinArray = await response.json();
         setGoblins(goblinArray);
       } catch (error) {
-        console.error("Error fetching goblin data:", error)
+        console.error("Error fetching goblin data:", error);
       }
     }
     fetchGoblins();
@@ -42,14 +42,14 @@ function App() {
   };
 
   const handleLogin = (user) => {
-    console.log(user)
+    console.log(user);
     setCurrentUser(user);
-  }
+  };
 
   const handleChangeUser = async (user) => {
-          setUsers([...users, user]);
-          setCurrentUser(user);
-  }
+    setUsers([...users, user]);
+    setCurrentUser(user);
+  };
 
   const handleDeleteUser = async (user) => {
     try {
@@ -59,43 +59,54 @@ function App() {
           "Content-Type": "application/json",
           Accept: "application/json",
         },
-    })
-    if (response.status === 200) {
-      const updatedUsers = users.filter((u) => u.username!== user.username);
-      setUsers(updatedUsers)
-      setCurrentUser(null);
-    } else {
-      console.log("Error deleting user:", response.status);
+      });
+      if (response.status === 200) {
+        const updatedUsers = users.filter((u) => u.username !== user.username);
+        setUsers(updatedUsers);
+        setCurrentUser(null);
+      } else {
+        console.log("Error deleting user:", response.status);
+      }
+    } catch (error) {
+      console.error("Error deleting user:", error);
     }
-    } catch(error) {
-      console.error("Error deleting user:", error)
-    }
-
-  }
+  };
 
   return (
     <BrowserRouter>
       <main>
-        <NavBar currentUser = {currentUser}/>
+        <NavBar currentUser={currentUser} />
         <Switch>
           <Route exact path="/">
-            <Home goblins = {goblins}/>
+            <Home goblins={goblins} />
           </Route>
           <Route exact path="/login">
-            <Login users = {users} handleAddUser = {handleAddUser} handleLogin = {handleLogin}/>
+            <Login
+              users={users}
+              handleAddUser={handleAddUser}
+              handleLogin={handleLogin}
+            />
           </Route>
           <Route exact path="/goblins">
             {goblins.length > 0 ? (
-              <GoblinContainer goblins = {goblins}/>
-              ) : (
-                <p>Loading goblins...</p>
-                )}
+              <GoblinContainer goblins={goblins} />
+            ) : (
+              <p>Loading goblins...</p>
+            )}
           </Route>
-          <Route path = "/goblins/:goblinName">
-            <GoblinDetails goblins = {goblins}/>
+          <Route path="/goblins/:goblinName">
+            <GoblinDetails goblins={goblins} />
           </Route>
-          <Route exact path = "/user/:username">
-            <UserPage users = {users} currentUser = {currentUser} handleChangeUser = {handleChangeUser} handleDeleteUser = {handleDeleteUser}/>
+          <Route exact path="/user/:username">
+            <UserPage
+              users={users}
+              currentUser={currentUser}
+              handleChangeUser={handleChangeUser}
+              handleDeleteUser={handleDeleteUser}
+            />
+          </Route>
+          <Route exact path="/date">
+            <Game currentUser={currentUser} goblins={goblins} />
           </Route>
         </Switch>
       </main>
@@ -104,5 +115,3 @@ function App() {
 }
 
 export default App;
-
-
---
+++ client/src/components/Game.js
@@ -0,0 +1,228 @@
+import { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+
+function Game({ currentUser, goblins }) {
+  const [goblin, setGoblin] = useState(null);
+  const [score, setScore] = useState(0);
+  const [dates, setDates] = useState([]);
+  const [chosenDate, setChosenDate] = useState(null);
+  const [part, setPart] = useState(1);
+  const [partDesc, setPartDesc] = useState({});
+  const [dialogues, setDialogues] = useState([]);
+  const [responses, setResponses] = useState([]);
+  const [response, setResponse] = useState(null);
+  const [outcomeResult, setOutcomeResult] = useState(null);
+
+  const history = useHistory();
+
+  useEffect(() => {
+    if (currentUser) {
+      async function fetchDates() {
+        try {
+          const response = await fetch("http://127.0.0.1:5555/dates");
+          const data = await response.json();
+          setDates(data);
+        } catch (error) {
+          console.log("Error fetching dates", error);
+        }
+      }
+      fetchDates();
+    }
+  }, [currentUser]);
+  async function pickDate(date) {
+    console.log(date);
+    try {
+      const response = await fetch("http://127.0.0.1:5555/dialogues");
+      if (response.status === 200) {
+        const data = await response.json();
+        const usertraits = currentUser.personality_traits.map(
+          (trait) => trait.id
+        );
+        const dialogueOptions = data.filter((dialogue) =>
+          usertraits.includes(dialogue.trait_id)
+        );
+        const filteredDialogues = dialogueOptions.filter(
+          (dialogue) => dialogue.date_id === date.id
+        );
+        setDialogues(filteredDialogues);
+        console.log(filteredDialogues);
+      } else {
+        console.log("Error fetching dialogues", response.status);
+      }
+    } catch (error) {
+      console.log("Error fetching dialogues", error);
+    }
+    setChosenDate(date);
+    setPartDesc({
+      1: date.part_1,
+      2: date.part_2,
+      3: date.part_3,
+    });
+  }
+  async function handleDialogueClick(dialogue) {
+    if (responses.length === 0) {
+      try {
+        const resp = await fetch("http://127.0.0.1:5555/responses");
+        if (resp.status === 200) {
+          const data = await resp.json();
+          const dataResponses = data.filter(
+            (res) => res.goblin_id === goblin.id
+          );
+          const dataResponse = dataResponses.find(
+            (res) => res.dialogue_id === dialogue.id
+          );
+          setResponse(dataResponse);
+          setResponses(dataResponses);
+          setPart(part + 1);
+          if (dataResponse.outcome === true) {
+            setScore(score + 1);
+          }
+        } else {
+          console.log("Error fetching responses", response.status);
+        }
+      } catch (error) {
+        console.log("Error fetching responses", error);
+      }
+    } else if (part > 3) {
+      try {
+        const resp = await fetch("http://127.0.0.1:5555/outcomes");
+        if (resp.status === 200) {
+          const data = await resp.json();
+          const dataOutcomes = data.filter(
+            (outcome) => outcome.goblin_id === goblin.id
+          );
+          console.log(dataOutcomes);
+          if (score === 3) {
+            const dataOutcome = dataOutcomes.find(
+              (outcome) =>
+                outcome.date_id === chosenDate.id && outcome.result === true
+            );
+            setOutcomeResult(dataOutcome);
+          } else {
+            const dataOutcome = dataOutcomes.find(
+              (outcome) =>
+                outcome.date_id === chosenDate.id && outcome.result === false
+            );
+            setOutcomeResult(dataOutcome);
+          }
+        } else {
+          console.log("Error fetching outcomes", response.status);
+        }
+      } catch (error) {
+        console.log("Error fetching outcomes", error);
+      }
+    } else {
+      const newResponse = responses.find(
+        (response) => response.dialogue_id === dialogue.id
+      );
+      console.log(newResponse);
+      setResponse(newResponse);
+      setPart(part + 1);
+      if (newResponse.outcome === true) {
+        setScore(score + 1);
+      }
+    }
+  }
+  function endDate() {
+    if (score === 3) {
+      history.push(`/user/${currentUser.username}`);
+    } else {
+      history.push(`/user/${currentUser.username}`);
+    }
+  }
+
+  return (
+    <div>
+      {currentUser ? (
+        <>
+          {chosenDate ? (
+            <div>
+              <img src={goblin.img_url} alt={goblin.name} />
+              <br />
+              <h2>
+                {part === 1
+                  ? chosenDate.description
+                  : response
+                  ? response.response
+                  : ""}
+              </h2>
+              <br />
+              <h3>{partDesc[part]}</h3>
+              <br />
+              {part > 3 ? (
+                <>
+                  {score === 3 ? (
+                    <>
+                      <br />
+                      <h2>
+                        {outcomeResult ? outcomeResult.outcome_description : ""}
+                      </h2>
+                      <button onClick={endDate}>End Date 😘</button>
+                    </>
+                  ) : (
+                    <>
+                      <h2>
+                        {outcomeResult ? outcomeResult.outcome_description : ""}
+                      </h2>
+                      <button onClick={endDate}>End Date 😒</button>
+                    </>
+                  )}
+                </>
+              ) : (
+                <>
+                  {dialogues
+                    .filter((dialogue) => dialogue.date_part === part)
+                    .map((dialogue, index) => (
+                      <div>
+                        <br />
+                        <button onClick={() => handleDialogueClick(dialogue)}>
+                          {index + 1}
+                        </button>
+                        <h4>{dialogue.description}</h4>
+                        <br />
+                      </div>
+                    ))}
+                </>
+              )}
+            </div>
+          ) : (
+            <div>
+              {goblin ? (
+                <>
+                  <h3>What date would you like to go on?</h3>
+                  {dates.map((date, index) => (
+                    <button key={index} onClick={() => pickDate(date)}>
+                      {date.name}
+                    </button>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <h1>Who would you like to invite out?</h1>
+                  {goblins.map((goblin, index) => (
+                    <div key={index}>
+                      <br />
+                      <img src={goblin.img_url} alt={goblin.name} />
+                      <h3>{goblin.name}</h3>
+                      <button onClick={() => setGoblin(goblin)}>
+                        Pick Me 😘
+                      </button>
+                      <br />
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <h1>Please Log In First!</h1>
+          <Link to="/login">Login</Link>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Game;
