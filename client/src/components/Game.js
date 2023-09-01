814bd080a300ab9b0657ca4cd56a1e61d1b1d924
Grant Bruemmer <grantbruemmer@gmail.com>
2023-08-31 19:37:05 -0500

    stage"

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,144 +1,158 @@
-import { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Link, useHistory } from "react-router-dom"
 
-function Game({ currentUser, goblins, handleChangeUser }) {
+function Game({ currentUser, goblins, handleChangeUser}) {
     const [goblin, setGoblin] = useState(null);
     const [score, setScore] = useState(0);
-    const [dates, setDates] = useState([]);
-    const [chosenDate, setChosenDate] = useState(null);
+    const [dates, setDates] = useState([])
+    const [chosenDate, setChosenDate] = useState(null)
     const [part, setPart] = useState(1);
-    const [partDesc, setPartDesc] = useState({});
-    const [dialogues, setDialogues] = useState([]);
-    const [responses, setResponses] = useState([]);
-    const [response, setResponse] = useState(null);
-    const [outcomeResult, setOutcomeResult] = useState(null);
+    const [partDesc, setPartDesc] = useState({})
+    const [dialogues, setDialogues] = useState([])
+    const [responses, setResponses] = useState([])
+    const [response, setResponse] = useState(null)
+    const [outcomeResult, setOutcomeResult] = useState(null)
 
-    const history = useHistory();
+    const history = useHistory()
 
     useEffect(() => {
         if (currentUser) {
             async function fetchDates() {
                 try {
-                    const response = await fetch("http://127.0.0.1:5555/dates");
-                    const data = await response.json();
-                    setDates(data);
+                    const response = await fetch('http://127.0.0.1:5555/dates');
+                    const data = await response.json()
+                    setDates(data)
                 } catch (error) {
-                    console.log("Error fetching dates", error);
+                    console.log('Error fetching dates', error)
                 }
             }
-            fetchDates();
-        }
-    }, [currentUser]);
+            fetchDates()
+        };
+    }, [currentUser])
     async function pickDate(date) {
-        console.log(date);
+        console.log(date)
         try {
-            const response = await fetch("http://127.0.0.1:5555/dialogues");
+            const response = await fetch('http://127.0.0.1:5555/dialogues')
             if (response.status === 200) {
-                const data = await response.json();
-                const usertraits = currentUser.personality_traits.map(
-                    (trait) => trait.trait_id
-                );
-                const dialogueOptions = data.filter((dialogue) =>
-                    usertraits.includes(dialogue.trait_id)
-                );
-                const filteredDialogues = dialogueOptions.filter(
-                    (dialogue) => dialogue.date_id === date.id
-                );
-                setDialogues(filteredDialogues);
-                console.log(filteredDialogues);
+                const data = await response.json()
+                const usertraits = currentUser.personality_traits.map(trait => trait.id)
+                const dialogueOptions = data.filter(dialogue => usertraits.includes(dialogue.trait_id))
+                const filteredDialogues = dialogueOptions.filter(dialogue => dialogue.date_id === date.id)
+                setDialogues(filteredDialogues)
+                console.log(filteredDialogues)
             } else {
-                console.log("Error fetching dialogues", response.status);
+                console.log('Error fetching dialogues', response.status)
             }
+
         } catch (error) {
-            console.log("Error fetching dialogues", error);
+            console.log('Error fetching dialogues', error)
         }
-        setChosenDate(date);
+        setChosenDate(date)
         setPartDesc({
             1: date.part_1,
             2: date.part_2,
             3: date.part_3,
-        });
+        })
+
+    }
+    async function updateUser(updatedUser) {
+        try {
+            const response = await fetch(`http://127.0.0.1:5555/users/${updatedUser.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(updatedUser)
+            })
+            if (response.status === 200) {
+                const updatedUserData = await response.json()
+                handleChangeUser(updatedUserData)
+            } else {
+                console.log('Error updating user', response.status)
+            }
+        } catch (error) {
+            console.error('Error updating user', error)
+        }
     }
     async function handleDialogueClick(dialogue) {
+        console.log(part)
+        console.log(score)
         if (responses.length === 0) {
             try {
-                const resp = await fetch("http://127.0.0.1:5555/responses");
+                const resp = await fetch('http://127.0.0.1:5555/responses')
                 if (resp.status === 200) {
-                    const data = await resp.json();
-                    const dataResponses = data.filter(
-                        (res) => res.goblin_id === goblin.id
-                    );
-                    const dataResponse = dataResponses.find(
-                        (res) => res.dialogue_id === dialogue.id
-                    );
-                    setResponse(dataResponse);
-                    setResponses(dataResponses);
-                    setPart(part + 1);
+                    const data = await resp.json()
+                    const dataResponses = data.filter(res => res.goblin_id === goblin.id)
+                    const dataResponse = dataResponses.find(res => res.dialogue_id === dialogue.id)
+                    setResponse(dataResponse)
+                    setResponses(dataResponses)
+                    setPart(part + 1)
                     if (dataResponse.outcome === true) {
-                        setScore(score + 1);
+                        setScore(score + 1)
                     }
                 } else {
-                    console.log("Error fetching responses", response.status);
+                    console.log('Error fetching responses', response.status)
                 }
             } catch (error) {
-                console.log("Error fetching responses", error);
+                console.log('Error fetching responses', error)
             }
         } else if (part === 3) {
-            const newResponse = responses.find(
-                (response) => response.dialogue_id === dialogue.id
-            );
-            console.log(newResponse);
-            setResponse(newResponse);
-            setPart(part + 1);
+            const newResponse = responses.find(response => response.dialogue_id === dialogue.id)
+            console.log(newResponse)
+            setResponse(newResponse)
+            setPart(part + 1)
             if (newResponse.outcome === true) {
-                setScore(score + 1);
+                setScore(score + 1)
             }
             try {
-                const resp = await fetch("http://127.0.0.1:5555/outcomes");
+                const resp = await fetch('http://127.0.0.1:5555/outcomes')
                 if (resp.status === 200) {
-                    const data = await resp.json();
-                    const dataOutcomes = data.filter(
-                        (outcome) => outcome.goblin_id === goblin.id
-                    );
-                    console.log(dataOutcomes);
+                    const data = await resp.json()
+                    const dataOutcomes = data.filter(outcome => outcome.goblin_id === goblin.id)
+                    console.log(dataOutcomes)
                     if (score >= 2) {
-                        const dataOutcome = dataOutcomes.find(
-                            (outcome) =>
-                                outcome.date_id === chosenDate.id && outcome.result === true
-                        );
-                        setOutcomeResult(dataOutcome);
+                        const dataOutcome = dataOutcomes.find(outcome => outcome.date_id === chosenDate.id && outcome.result === true)
+                        const fieldName = `${goblin.name.toLowerCase()}_win`;
+                        const updatedUser = {
+                            id: currentUser.id,
+                            [fieldName]: currentUser[fieldName] + 1
+                        }
+                        updateUser(updatedUser)
+                        console.log(updatedUser)
+                        setOutcomeResult(dataOutcome)
                     } else {
-                        const dataOutcome = dataOutcomes.find(
-                            (outcome) =>
-                                outcome.date_id === chosenDate.id && outcome.result === false
-                        );
-                        setOutcomeResult(dataOutcome);
+                        const dataOutcome = dataOutcomes.find(outcome => outcome.date_id === chosenDate.id && outcome.result === false)
+                        const fieldName = `${goblin.name.toLowerCase()}_win`;
+                        const updatedUser = {
+                            id: currentUser.id,
+                            [fieldName]: currentUser[fieldName] - 1
+                        }
+                        updateUser(updatedUser)
+                        console.log(updatedUser)
+                        setOutcomeResult(dataOutcome)
                     }
                 } else {
-                    console.log("Error fetching outcomes", response.status);
+                    console.log('Error fetching outcomes', response.status)
                 }
             } catch (error) {
-                console.log("Error fetching outcomes", error);
+                console.log('Error fetching outcomes', error)
             }
         } else {
-            const newResponse = responses.find(
-                (response) => response.dialogue_id === dialogue.id
-            );
-            console.log(newResponse);
-            setResponse(newResponse);
-            setPart(part + 1);
+
+            const newResponse = responses.find(response => response.dialogue_id === dialogue.id)
+            console.log(newResponse)
+            setResponse(newResponse)
+            setPart(part + 1)
             if (newResponse.outcome === true) {
-                setScore(score + 1);
+                setScore(score + 1)
             }
         }
     }
     function endDate() {
-        if (score === 3) {
-            history.push(`/user/${currentUser.username}`);
-        } else {
-            history.push(`/user/${currentUser.username}`);
-        }
+            history.push(`/user/${currentUser.username}`)
     }
+
 
     return (
         <div>
